7acb192abdc07525621ba0c9ad9b8246d3c52985
wags <i.doyle.w@gmail.com>
2025-12-05 22:53:25 -0800

    fix

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,13 +1,8 @@
 'use client';
 
-import type { Metadata } from "next";
 import { NextStudio } from "next-sanity/studio";
 
 import config from "../../../../sanity/sanity.config";
-
-export const metadata: Metadata = {
-  title: "Studio | Studio Tak",
-};
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
