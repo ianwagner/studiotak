b1053a8a092cf934ad07ecbc2a879d08156e9b91
wags <i.doyle.w@gmail.com>
2025-12-05 16:22:45 -0800

    fix

--- sanity/schemaTypes/utils/editorialGroups.ts
+++ sanity/schemaTypes/utils/editorialGroups.ts
@@ -4,6 +4,6 @@ export const ADVANCED_GROUP = 'advanced' as const
 export const editorialGroups = [
   {name: ESSENTIALS_GROUP, title: 'Essentials', default: true},
   {name: ADVANCED_GROUP, title: 'Advanced'},
-] as const
+] satisfies Array<{name: string; title: string; default?: boolean}>
 
 export type EditorialGroupName = (typeof editorialGroups)[number]['name']
